03000746c8ebed35ad7a2aa3cffc64440d1daa57
quocquoc123 <120048015+quocquoc123@users.noreply.github.com>
2022-12-08 02:08:19 +0700

    Add files via upload
    quoc

---
+++ present.js
@@ -0,0 +1,43 @@
+var to = 'Ngọc Ánh 🥰';
+var gift_url = 'https://www.facebook.com/profile.php?id=100015090369769';
+var gift_image_url = 'C:\Users\Admin\Pictures\IMG_20211017_115420 (1).jpg';
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+
+  var _giftLink, 
+      _giftImg;
+
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+
+
+
+  nametag.innerText = to;
+}
+
+init();
